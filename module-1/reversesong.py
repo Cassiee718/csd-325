dc3a17df639580fc35a973d58529c88eef2aac34
Cassie Noel <cassienoel@Cassies-MacBook-Pro.local>
2025-01-12 13:49:49 -0500

    pushing 1/12/25 2.2

--- module-1/reversesong.py
+++ module-1/reversesong.py
@@ -8,14 +8,14 @@ def bottle_song(bottles):
 			" Take one down and pass it around,")
 			
 		else:
-			print(f"1 bottle of beer on the wall, 1 bottle of beer"
+			print(f"1 bottle of beer on the wall, 1 bottle of beer."
 			" Take one down and pass it around, no more bottles of beer on"
 			" the wall.")
 		bottles -=1	
 			
 	print("No more bottles of beer on the wall, no more bottles of beer."
-	" It's time to buy more bottles of beer,") 
-	print("99 bottles of beer on the wall")
+	" It's time to buy more bottles of beer.") 
+	
 	
 def main():
 #get the number of bottles from user	
